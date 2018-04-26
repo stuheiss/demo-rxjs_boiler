6c3fe5970229f08615c85dedcba29938cb0d69e5
Stu Heiss <sheiss@highlandsolutions.com>
2018-04-26 09:03:57 -0500

    observables from events

---
+++ readme.txt
@@ -0,0 +1,3 @@
+https://github.com/bradtraversy/rxjs_boiler
+https://www.youtube.com/watch?v=ei7FsoXKPl0
+https://www.eduonix.com/courses/Web-Development/learn-reactivex-from-ground-up
--- src/app.js
+++ src/app.js
@@ -1,4 +1,36 @@
-import $ from 'jquery';
-import Rx from 'rxjs/Rx';
+import $ from 'jquery'
+import Rx from 'rxjs/Rx'
 
-console.log('RxJS Boiler Running...');
+//console.log('RxJS Boiler Running...');
+
+const btn = $('#btn')
+const input = $('#input')
+const output = $('#output')
+const btnpos = $('#btnpos')
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click')
+const inputStream$ = Rx.Observable.fromEvent(input, 'keyup')
+const moveStream$ = Rx.Observable.fromEvent(document, 'mousemove')
+
+btnStream$.subscribe(
+  e =>
+    console.log(
+      e.target.offsetHeight,
+      e.target.offsetLeft,
+      e.target.offsetTop,
+      e.target.offsetWidth
+    ),
+  err => console.log(err),
+  () => console.log('btn done')
+)
+
+inputStream$.subscribe(
+  e => output.html(e.target.value),
+  err => console.log(err),
+  () => console.log('input done')
+)
+
+moveStream$.subscribe(
+  e => btnpos.html(JSON.stringify({ x: e.clientX, y: e.clientY })),
+  err => console.log(err),
+  () => console.log('input done')
+)
